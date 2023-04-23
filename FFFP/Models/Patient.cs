bec0624dc527f0009b1c848e8ee508b6f7879a0c
Dulina Gunasekara <83606238+Dulinag@users.noreply.github.com>
2023-04-22 21:46:56 -0400

    Add files via upload

---
+++ FFFP/Models/Patient.cs
@@ -0,0 +1,10 @@
+public class Patient
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    public string? Gender { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+}
---
+++ FFFP/MyFirstAPIDBContext.cs
@@ -0,0 +1,24 @@
+﻿using FFFP.Models;
+using Microsoft.EntityFrameworkCore;
+namespace FFFP.Models
+{
+    public class MyFirstAPIDBContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public MyFirstAPIDBContext(DbContextOptions<MyFirstAPIDBContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = Configuration.GetConnectionString("MyHospitalDB");
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+        public DbSet<Patient> Patients { get; set; } = null!;
+        public DbSet<Doctor> Doctors { get; set; } = null!;
+
+        public DbSet<Appointment> Appoinments { get; set; } = null!;
+    }
+}
