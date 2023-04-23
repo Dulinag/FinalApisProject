e0ea7a4cbb79cf824b0ddc450b9d1ab0174e85a6
Dulina Gunasekara <83606238+Dulinag@users.noreply.github.com>
2023-04-22 21:44:49 -0400

    Add files via upload

---
+++ WebApplication1/WebApplication1/Models/Singer.cs
@@ -0,0 +1,9 @@
+namespace FFFP.Models
+{
+    public class Singer
+    {
+        public int SingerId { get; set; }
+        public string? SingerName { get; set; }
+        public Albums? Albums { get; set; }
+    }
+}
