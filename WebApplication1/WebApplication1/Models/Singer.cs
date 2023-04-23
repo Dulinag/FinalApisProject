7881cf31fd0c630f68852cb8de2829c56231f12b
Dulina Gunasekara <83606238+Dulinag@users.noreply.github.com>
2023-04-22 21:45:30 -0400

    Delete WebApplication1 directory

--- WebApplication1/WebApplication1/Models/Singer.cs
+++
@@ -1,9 +0,0 @@
-namespace FFFP.Models
-{
-    public class Singer
-    {
-        public int SingerId { get; set; }
-        public string? SingerName { get; set; }
-        public Albums? Albums { get; set; }
-    }
-}
