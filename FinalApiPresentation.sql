890e0ec0bc7894c3d47ac5c72983d6ee370fd301
Dulina Gunasekara <83606238+Dulinag@users.noreply.github.com>
2023-05-02 18:59:14 -0400

    Add files via upload

---
+++ FinalApiPresentation.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE MyHospitalDB9;
+
+USE MyHospitalDB9;
+
+
+
+CREATE TABLE Doctors (
+    DoctorId INT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Specialization VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL
+);
+
+
+CREATE TABLE Patients (
+    PatientId INT PRIMARY KEY AUTO_INCREMENT,
+    Name VARCHAR(255) NOT NULL,
+    Age INT NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    Email VARCHAR(255) NOT NULL,
+    PhoneNumber VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Appointments (
+AppointmentId INT NOT NULL AUTO_INCREMENT,
+Date DATE NOT NULL,
+StartTime TIME NOT NULL,
+EndTime TIME NOT NULL,
+DoctorId INT NOT NULL,
+PatientId INT NOT NULL,
+PRIMARY KEY (AppointmentId),
+FOREIGN KEY (DoctorId) REFERENCES doctor(DoctorId),
+FOREIGN KEY (PatientId) REFERENCES patient(PatientId)
+);
+
